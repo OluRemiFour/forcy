60d00ff8b02e28610b924c95901db3a95de7d972
OluRemiFour <lekanayoola04@gmail.com>
2025-01-13 09:13:27 -0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -25,35 +25,35 @@ const Page = () => {
     {
       name: "John, Management",
       brand: "Starks",
-      image: "/john.svg",
+      image: "/john.jpeg",
       details:
         "Partnering with Forcythe was like finding a hidden gem. Their genuine interest in our success was palpable, and the continuous support post-launch has been a testament to their commitment. They’ve become more than a service provider; they’re a trusted ally.",
     },
     {
       name: "Testimony, Co-founder",
       brand: "ExecutivePros",
-      image: "/executive-pro-ceo.svg",
+      image: "/executive-pro-ceo.jpeg",
       details:
         "The team understood the assignment and delivered very well. One of the things that stood them out was how they took our concepts and turned it into visually appealing designs that caught the eyes of our clients and made increased web engagements. Kudos!",
     },
     {
       name: "Edwin, Former CTO",
       brand: "Stac AI",
-      image: "/edwin.svg",
+      image: "/edwin.jpeg",
       details:
         "Forcythe is seriously amazing when it comes to coming up with new ideas. They took our rough ideas and turned them into something incredible online. Their team’s commitment to our vision was evident every step of the wa",
     },
     {
       name: "Iwaria, Founder",
       brand: "Iwaria",
-      image: "/iwaria-founder.svg",
+      image: "/wwwl.jpeg",
       details:
         "The moment we engaged Forcythe, it was clear they were in a league of their own. Their strategic approach to our project not only enhanced our online platform but also enriched our brand’s story, captivating our audience like never before.",
     },
     {
       name: "Christiana, Founder",
       brand: "Beaupreneur",
-      image: "/christina.svg",
+      image: "/christina.webp",
       details:
         "I’m beyond impressed with the dedication Forcythe showed. They didn’t just deliver—they educated us, involving us in the creative process. The result? A website that truly feels like ours and speaks directly to our customers’ hearts.",
     },
